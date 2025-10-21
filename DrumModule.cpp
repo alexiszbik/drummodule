6580e1a9a0ef2aba640853d3703e87ef4a405773
Alexis ZBIK <alexis.zbik@gmail.com>
2025-10-21 20:00:33 +0200

    Chores : test some percussive tones

--- DrumModule.cpp
+++ DrumModule.cpp
@@ -9,13 +9,48 @@ using namespace daisysp;
 // Declare a DaisySeed object called hardware
 DaisySeed  hardware;
 Oscillator osc;
-AdEnv      env;
+
+class Decay {
+public:
+    void Init(float sampleRate) {
+        this->sampleRate = sampleRate;
+    }
+
+    void SetTime(float time) {
+        time = fmaxf(time, 0.000001f);
+        increment = 1.f / (time * sampleRate);
+    }
+
+    float Process() {
+        if (trig) {
+            trig = false;
+            value = 1.0f;
+        }
+        if (value > 0) {
+            value = value - increment;
+            return value * value;
+        } else {
+            return 0;
+        }
+    }
+
+    void Trig() {
+        trig = true;    
+    }
+private:
+    float sampleRate = 44100;
+    float value = 0.f; 
+    bool trig = false;
+    float increment = 0.001;
+};
+
+bool gate = false;
+float basePitch = 60;
+
+Decay pitchEnv;
+Decay ampEnv;
 
 MidiUartHandler midi;
-
-Switch button1;
-
-int note = 60;
 
 void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                    AudioHandle::InterleavingOutputBuffer out,
@@ -27,7 +62,12 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     {
         //Get the next envelope value
         //get the next oscillator sample
-        float osc_out = osc.Process();
+    
+        int pitch = basePitch + pitchEnv.Process() * 24;
+
+        osc.SetFreq(mtof(pitch));
+
+        float osc_out = osc.Process() * ampEnv.Process();
 
         //Set the left and right outputs
         out[i]     = osc_out;
@@ -41,12 +81,15 @@ void HandleMidiMessage(MidiEvent m)
 {
     if (m.type == NoteOn) {
         NoteOnEvent noteOn = m.AsNoteOn();
-        note = noteOn.note;
-        osc.SetFreq(mtof(note));
-        osc.SetAmp(noteOn.velocity > 0);
+        basePitch = noteOn.note;
+        gate = noteOn.velocity > 0;
+        if (gate) {
+            ampEnv.Trig();
+            pitchEnv.Trig();
+        }
     }
     else if (m.type == NoteOff) {
-        osc.SetAmp(0);
+         gate = false;
     } 
 }
 
@@ -78,6 +121,12 @@ int main(void)
 
     //Start the adc
     //hardware.adc.Start();
+    //Good settings for a TOM :)
+    pitchEnv.Init(samplerate);
+    pitchEnv.SetTime(0.02);
+
+    ampEnv.Init(samplerate);
+    ampEnv.SetTime(0.2);
 
     //Start calling the audio callback
     hardware.StartAudio(AudioCallback);
